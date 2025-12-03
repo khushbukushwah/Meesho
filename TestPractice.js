99d7e89bffa2ff7c6b20faf66384a4e345834b15
Kushbukushwah <khushbukushwah27@gmail.com>
2025-12-03 13:20:49 +0530

    "My Code changes"

--- TestPractice.js
+++ TestPractice.js
@@ -5,3 +5,17 @@ if (age < 18) {
 } else {
   console.log("Adult");
 }
+
+let myage = 10;
+if (myage < 18) {
+  console.log("You are minor");
+} else {
+  console.log("You are now Adult");
+}
+
+let myage1 = 20;
+if (myage1 < 18) {
+  console.log(" Hey thers You are minor");
+} else {
+  console.log("Hey you are now Adult");
+}
